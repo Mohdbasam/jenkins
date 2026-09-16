b1011314acef1006a115f160b2b88fdeaaa8f65d
Muhammed Basam K <mohdbasamoffl@gmail.com>
2026-09-16 21:57:51 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,10 +3,8 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Maven Pipeline!");
-
-        int result = add(10, 20);
-        System.out.println("10 + 20 = " + result);
+        System.out.println("Hello from Jenkins!");
+        System.out.println("This application is deployed to GitHub Packages.");
     }
 
     public static int add(int a, int b) {
